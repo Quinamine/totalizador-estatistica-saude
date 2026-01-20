f946b234bc323b9a0521dff0cfb06ca6e721eda2
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2026-01-20 19:11:23 +0200

    chore(build): setup production pipeline and project directory structure
    - Configure Sass and PostCSS (`Autoprefixer`, `CSSNano`) to minify and prefix styles.
    - Setup `html-minifier-terser` and `Terser` for asset optimization.
    - Create initial `index.html` linked to `.min.css` and `.min.js` files.
    - Add `.gitignore` to exclude `node_modules` and build artifacts.

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	plugins: [
+		require("postcss-preset-env")({
+			stage: 2,
+			features: {
+				"oklab-function": false,
+				"custom-properties": false,
+			},
+			}),
+		require("autoprefixer"),
+		require("cssnano")({
+			preset: "default",
+		}),
+	],
+};
